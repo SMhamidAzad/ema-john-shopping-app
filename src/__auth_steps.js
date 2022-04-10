577032bcc5cc459ce8f6ab63d8eb98d933bbf2f1
SMhamidAzad <hamidazad020@gmail.com>
2022-04-10 12:50:05 +0600

    login form and loading

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -5,6 +5,10 @@
 4. firebase build> authentication>get started> enable email provider
 5. create login,signup components and setup route
 6. attach form field handler and submit handler
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword from firebase hooks
+9. if user is created, redirect to the expected page
+10. useSignInWithEmailAndPassword use for sign in
 */
 
 /**
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import './SignUp.css'
 import GoogleLogo from './../../img/google.svg'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init'
 
 const SignUp = () => {
     const [email,setEmail]=useState('')
@@ -9,6 +11,8 @@ const SignUp = () => {
     const [confirmpass,setConfirmPass]=useState('')
     const [error,setError]=useState('')
 
+    const [createUserWithEmailAndPassword, hookError,user] = useCreateUserWithEmailAndPassword(auth)
+    const navigate = useNavigate()
     const handleEmailBlur = event =>{
         setEmail(event.target.value)
     }
@@ -18,11 +22,20 @@ const SignUp = () => {
     const handleConfirmPassBlur = event =>{
         setConfirmPass(event.target.value)
     }
+    if(user){
+       navigate('/shop')
+    }
     const handleCreateUser = event =>{
         event.preventDefault();
         if(password!==confirmpass){
             setError('Your two passwords did not match')
         }
+        if(password.length<6){
+            setError('password must be 6 character or longer')
+            return;
+        }
+        createUserWithEmailAndPassword(email,password)
+       
     }
     return (
         <div className='form-container'>
@@ -42,6 +55,7 @@ const SignUp = () => {
                         <input onBlur={handleConfirmPassBlur} type="password" name="confirm-password" id="" required />
                     </div>
                     <p style={{color: 'red'}}>{error}</p>
+                    <p style={{color: 'red'}}>{hookError}</p>
                     <input className='form-submit' type="submit" value="Sign Up" />
                 </form>
                 <p style={{ textAlign: 'center', margin: '0' }}>
